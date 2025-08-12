d48068c88c62738e740132b88dfe9cae7851efcf
OmKadam-1 <kom110798@gmail.com>
2025-08-12 20:59:05 +0530

    js file completed

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-const element1=0;
+ function dialog() {
+            const element1 = document.getElementById("dialog");
+            element1.style.display = "flex";
+            const element2 = document.getElementById("container");
+            const element3 = document.getElementById("button")
+            element3.style.display = "none";
+            element2.style.backgroundColor = "rgba(0, 0, 0, 0.7)"
+
+
+
+        }
+        function closedialog() {
+            const element1 = document.getElementById("dialog");
+            const element2 = document.getElementById("container");
+            element1.style.display = "none";
+            element2.style.backgroundColor = "white";
+            const element3 = document.getElementById("button")
+            element3.style.display = "flex";
+        }
